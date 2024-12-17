97738cb18148245930196b32bb6362f8a693d4a8
korichh <akopyankor@gmail.com>
2024-12-17 23:04:57 +0200

    static serve

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import "dotenv/config"
+import path from "path"
 import express from "express"
 import { setSession } from "./middleware"
 import adminRouter from "./admin/routes"
@@ -8,9 +9,8 @@ import sequelize from "./config/connection"
 const app = express()
 const PORT = process.env.PORT
 
-// import path from "path"
-// app.use("/", express.static(path.join(process.cwd(), "client/public")))
-// app.use("/admin", express.static(path.join(process.cwd(), "client/admin/public")))
+app.use("/", express.static(path.join(process.cwd(), "client/public")))
+app.use("/admin", express.static(path.join(process.cwd(), "client/admin/public")))
 
 app.set('trust proxy', 1)
 app.use(express.json())
